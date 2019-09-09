b1347c3f2c5a79470939240f9e6bd88f9b6d8823
raseliet <54890928+raseliet@users.noreply.github.com>
2019-09-09 12:24:15 +0300

    HTML su PHP1

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<?php
-;?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Aš valdau HTML!</title>
+    <title>Aš valdau HTML! <?php print 'Aš valdau HTML!'; ?> </title>
+    
 </head>
 <body>
     <h1>Rasa - HTML asė!</h1>
+    <h1><?php print 'Rasa - HTML asė!'; ?></h1>
     <p>Moku kurti satinį turinį.</p>
+    <p><?php print 'Moku kurti satinį turinį.'; ?></p>
+    <?php print $date = date('m/d/Y h:i:s a', time()); ;?>
 </body>
 </html>
