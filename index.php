79b51e8b3923dc3b54a65142b9349a638ef14879
raseliet <rasele27@gmail.com>
2019-09-10 15:24:26 +0300

    Čia yra komentaras,

--- index.php
+++ index.php
@@ -1,16 +1,35 @@
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Aš valdau HTML! <?php print 'Aš valdau HTML!'; ?> </title>
-    
-</head>
-<body>
-    <h1>Rasa - HTML asė!</h1>
-    <h1><?php print 'Rasa - HTML asė!'; ?></h1>
-    <p>Moku kurti satinį turinį.</p>
-    <p><?php print 'Moku kurti satinį turinį.'; ?></p>
-    <?php print $date = date('m/d/Y h:i:s a', time()); ;?>
-</body>
+/**
+ * komentaras
+ * [
+ * gtjj
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+print 'Labas';
+//print 'Iki';
+
+?>
+
+
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php print 'Aš ' . date('l') . ' ir PHP'; ?></title>
+    </head>
+    <body>
+        <h1>
+            Rasa - HTML <?php print 'ir PHP'; ?> asė jau nuo <?php print date('Y') . ' metų'; ?>
+        </h1>
+        <p>
+            Viskas prasidėjo <?php print date('m') . ' mėnesio, ' . date('d') . ' dieną!'; ?> 
+        </p>
+    </body>
 </html>
