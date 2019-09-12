7bcff14b68b0fb229010719dc74bd3a68d60b054
raseliet <rasele27@gmail.com>
2019-09-12 09:57:13 +0300

    Užduotis: sukurti po $variable kiekvienam tipui ir išspausdinti.

--- index.php
+++ index.php
@@ -1,30 +1,13 @@
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Bomb img game, and date()</title>
-        <style>
-            .bomb-img {
-                background-image: url(http://pngimg.com/uploads/bomb/bomb_PNG16.png);
-                background-size: cover;
-                width: 1<?php print date('s') ;?>px;
-                height: 1<?php print date('s') ;?>px;
-            }
-            
-            .bomb-img-00 {
-                background-image: url(https://miro.medium.com/max/1005/1*XIajoiQj2tQfdNWZ7_PS5w.jpeg);
-                background-size: cover;
-                width: 100px;
-                height: 100px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="bomb-img  bomb-img-<?php print date('s') ;?> "></div>
-        <div><?php print date('s'); ?></div>
-        
-    </body>
+<body>
+
+<h4>Sukurti po $variable kiekvienam tipui ir išspausdinti:</h4>
+<ul>
+<li>$string = 'Tai yra tekstas'</li>
+<li>$int = 15</li>
+<li>$bool = true</li>
+<li>$null = nothing</li>
+</ul>
+
+</body>
 </html>
-
-
-
-
