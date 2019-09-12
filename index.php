d770e79300af878ea918009eb2f3c86c3d1968d6
raseliet <rasele27@gmail.com>
2019-09-12 14:29:56 +0300

    Laikas su kintamaisiais.

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
-<?php
-$siukslines_turis = 40; //litrų
-$siuksliu_turis_per_d = 15; //litrų
-$max_kaupo_turis = rand(0, 45) ; //litrų
-$po_kiek_dienų = floor(($siukslines_turis + $max_kaupo_turis) / $siuksliu_turis_per_d);
-$data = date('Y-m-d', strtotime("+$po_kiek_dienų days"));
-$atsakymas = "Po $po_kiek_dienų ($data) pirk gėlių ir šampano, jeigu nori išvengti konflikto.";
-?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Užduotis - po kelių dienų kils barnis?</title>
-    </head>
-    <body>
-        <h1><?php print $atsakymas ;?></h1>
-    </body>
-</html>
+<?php $laikas = date('s'); 
+$number_1 = 15;
+$number_2 = 97;
+
+$number_3 = $number_1 % $number_2;
+print $number_3;
+print $laikas . '<br/>';
+print $laikas % 2;
+
