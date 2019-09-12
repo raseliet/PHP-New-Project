338cc8cbb14f5f85da3c3e34bd16f5277061c736
raseliet <rasele27@gmail.com>
2019-09-12 12:44:45 +0300

    Užduotis: Po keliu dienu kils barnis?

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <?php
-​
-$img = 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg';
-​
+$siukslines_turis = 40; //litrų
+$siuksliu_turis_per_d = 15; //litrų
+$max_kaupo_turis = rand(0, 45) ; //litrų
+$po_kiek_dienų = floor(($siukslines_turis + $max_kaupo_turis) / $siuksliu_turis_per_d);
+$data = date('Y-m-d', strtotime("+$po_kiek_dienų days"));
+$atsakymas = "Po $po_kiek_dienų ($data) pirk gėlių ir šampano, jeigu nori išvengti konflikto.";
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
+        <title>Užduotis - po kelių dienų kils barnis?</title>
     </head>
     <body>
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
+        <h1><?php print $atsakymas ;?></h1>
     </body>
 </html>
-
-
-
-
-
-
-
