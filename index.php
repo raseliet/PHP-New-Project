205f74aa8571890667cdabec09ff36d37449174b
raseliet <rasele27@gmail.com>
2019-09-13 14:27:47 +0300

    Sunny or cloudy - atnaujinta.

--- index.php
+++ index.php
@@ -1,27 +1,38 @@
 <?php
-$grizai_velai = rand(0, 1);
-$grizai_isgeres = rand(0, 1);
+$sunny = rand(0, 1);
+$cloudy = rand(0, 1);
 
-if ($grizai_velai && !$grizai_isgeres) {
-   $pasekmes='Grizai velai';
-}else if($grizai_velai && $grizai_isgeres) {
-   $pasekmes='Grizai velai ir isgeres';
-}else if(!$grizai_velai && $grizai_isgeres){
-    $pasekmes='Grizai isgeres';
-}else{
-    $pasekmes='Nieko nepadarei';
+
+if ($sunny) {
+    $koks_siandien_oras = 'Sauleta';
+    $class = 'sunny';
+} else {
+    $koks_siandien_oras = 'Debesuota';
+    $class = 'cloudy';
 }
-
-$aplinkybes='Situacija: ' . $pasekmes;
-
 ?>
-
 <html>
     <head>
-        <title>Buitinė skaičiuoklė</title>  
+        <meta charset="UTF-8">
+        <style>
+            .sunny {
+                background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQREayfm8mV1QonfqBx8pFVuZ5_DygVUi13JzxQFTzs-tIUwRDv);
+                background-size: cover;
+                width: 100px;
+                height: 100px;
+            }
+            .cloudy {
+                background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr4luZFsUQeEZ5lIexKU8fINIQPP9LSymaWwGapeXvrrleqgJq);
+                background-size: cover;
+                width: 100px;
+                height: 100px;
+            }
+
+        </style>
     </head>
     <body>
-        <h1>Buitine skaiciuokle </h1>
-        <h2><?php print $aplinkybes; ?></h2>
+        <h1>Sauleta ar debesuota</h1>
+        <div class="<?php print $class; ?>"></div> 
+        <p><?php print $koks_siandien_oras; ?></p>
     </body>
 </html>
