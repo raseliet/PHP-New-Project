e47c99d18bfdcc1d67e84872442ce5750e5975a5
raseliet <rasele27@gmail.com>
2019-09-13 10:47:30 +0300

    Uzduotis 2: kuro skaiciuokle su if - iperkama.

--- index.php
+++ index.php
@@ -1,36 +1,34 @@
 <?php
-$distance = rand(50, 200); // km
-$consumption = 7.5;        // 1/100km
-$price = 1.3;              // Eur/l
+$distance = rand(100, 200);
+$consumption = 7.5;
+$price_1 = 1.3;
+$my_money = 100;
 
-$fuel_total = $consumption * $distance / 100;
-$price_trip = $fuel_total * $price;
+$fuel_total = $distance * $consumption / 100;
+$price_trip = $fuel_total * $price_1;
 
-$antraste = 'Kelionės skaičiuoklė';
-$kiek_nuvaziuota_km = 'Nuvažiuota distancija: ' . $distance . ' km.';
-$kiek_sunaudota_kuro = 'Sunaudota: ' . $fuel_total . ' l. kuro.';
-$keliones_sanaudos = 'Kaina: ' . $price_trip . ' eur.';
-        
+if ($price_trip <= $my_money) {
+    $iperkama = 'įperkama.';
+} else {
+    $iperkama = 'neįperkama.';
+};
+
+
+$title = 'Kelionės skaičiuoklė';
+$text = 'Suskaičiuoti ir išspausdinti:';
+$text_1 = 'Turimi pinigai:' . $my_money . ' eur.';
+$text_2 = 'Išvada: kelionė ' . $iperkama;
 ?>
-        
 <html>
-    <head> 
-        <meta charset="UTF - 8">
-        <title>Suskaičiuoti ir išspausdinti</title>
-
+    <head>
+        <title><?php print $title; ?></title>  
     </head>
     <body>
-        <h1><?php print $antraste; ?></h1>
+        <h1><?php print $text; ?></h1>
         <ul>
-            <li><?php print $kiek_nuvaziuota_km; ?></li>
-            <li><?php print $kiek_sunaudota_kuro; ?></li>
-            <li><?php print $keliones_sanaudos; ?></li>
-        </ul>  
-
+            <li><?php print $text_1; ?></li>
+        </ul>
+        <hr>
+        <p><?php print $text_2; ?></p>
     </body>
-</html> 
-
-
-
-
-
+</html>
