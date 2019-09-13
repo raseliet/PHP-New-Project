d2dbed1232f093d8eec7aee834e0f333ebafecf1
raseliet <rasele27@gmail.com>
2019-09-13 11:05:54 +0300

    Uzduotis 2: kuro skaiciuokle su if - iperkama - atnaujinta.

--- index.php
+++ index.php
@@ -7,6 +7,11 @@ $my_money = 100;
 $fuel_total = $distance * $consumption / 100;
 $price_trip = $fuel_total * $price_1;
 
+$antraste = 'Kelionės skaičiuoklė';
+$kiek_nuvaziuota_km = 'Nuvažiuota distancija: ' . $distance . ' km.';
+$kiek_sunaudota_kuro = 'Sunaudota: ' . $fuel_total . ' l. kuro.';
+$keliones_sanaudos = 'Kaina: ' . $price_trip . ' eur.';
+
 if ($price_trip <= $my_money) {
     $iperkama = 'įperkama.';
 } else {
@@ -18,6 +23,13 @@ $title = 'Kelionės skaičiuoklė';
 $text = 'Suskaičiuoti ir išspausdinti:';
 $text_1 = 'Turimi pinigai:' . $my_money . ' eur.';
 $text_2 = 'Išvada: kelionė ' . $iperkama;
+
+
+if ($price_trip <= $my_money) {
+    $iperkama = 'įperkama.';
+} else {
+    $iperkama = 'neįperkama.';
+};
 ?>
 <html>
     <head>
@@ -26,6 +38,9 @@ $text_2 = 'Išvada: kelionė ' . $iperkama;
     <body>
         <h1><?php print $text; ?></h1>
         <ul>
+            <li><?php print $kiek_nuvaziuota_km; ?></li>
+            <li><?php print $kiek_sunaudota_kuro; ?></li>
+            <li><?php print $keliones_sanaudos; ?></li>
             <li><?php print $text_1; ?></li>
         </ul>
         <hr>
