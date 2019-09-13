8e95abd5de81070391e4f857103127b461b9c919
raseliet <rasele27@gmail.com>
2019-09-13 10:15:33 +0300

    Uzduotis 1: kuro skaiciuokle.

--- index.php
+++ index.php
@@ -1,44 +1,36 @@
 <?php
-$laikas = date('s');
+$distance = rand(50, 200); // km
+$consumption = 7.5;        // 1/100km
+$price = 1.3;              // Eur/l
 
+$fuel_total = $consumption * $distance / 100;
+$price_trip = $fuel_total * $price;
 
-$laikas_reversed = 59 - $laikas;
-
-
-
-
-
+$antraste = 'Kelionės skaičiuoklė';
+$kiek_nuvaziuota_km = 'Nuvažiuota distancija: ' . $distance . ' km.';
+$kiek_sunaudota_kuro = 'Sunaudota: ' . $fuel_total . ' l. kuro.';
+$keliones_sanaudos = 'Kaina: ' . $price_trip . ' eur.';
+        
 ?>
+        
 <html>
     <head> 
         <meta charset="UTF - 8">
-        <title>Bomba su Css ir kintamaisiais</title>
-        <style>
-            .square {
-                height: 200px;
-                width: 200px;
-                background-color: #555;
-                text-align: center;
-            }
-            
-            .time0 {
-                background-color: red;
-            }
-                    
-          
-        </style>
+        <title>Suskaičiuoti ir išspausdinti</title>
+
     </head>
     <body>
-        <div class="square time<?php print $laikas_reversed; ?>">
-            <h1><?php print $laikas_reversed; ?></h1>
-            
-        </div>
-        
+        <h1><?php print $antraste; ?></h1>
+        <ul>
+            <li><?php print $kiek_nuvaziuota_km; ?></li>
+            <li><?php print $kiek_sunaudota_kuro; ?></li>
+            <li><?php print $keliones_sanaudos; ?></li>
+        </ul>  
+
     </body>
 </html> 
 
-      
-        
-   
-    
-    
+
+
+
+
