da848828227176966d50962c6be529a8e305cf9e
raseliet <rasele27@gmail.com>
2019-09-15 19:13:53 +0300

    N.D.: Koks šiandien oras?

--- index.php
+++ index.php
@@ -1,53 +1,47 @@
 <?php
-$randomiser = rand(0, 2);
-
-if ($randomiser === 0) {
-    $coffee = 'black-coffee';
-} elseif ($randomiser === 1) {
-    $coffee = 'latte';
+$sunny = rand(0, 1);
+$cloudy = rand(0, 1);
+$rainy = rand(0, 1);
+if ($sunny) {
+    $koks_siandien_oras = 'Saulėta';
+    $class = 'sunny';
+} elseif ($cloudy) {
+    $koks_siandien_oras = 'Debesuota';
+    $class = 'cloudy';
 } else {
-    $coffee = 'tea';
-} 
-    
+    $koks_siandien_oras = 'Lyja';
+    $class = 'rainy';
+
+}
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Ka gersi - trys opcijos</title>
-        <link rel="stylesheet" href="includes/normalize.css">
         <style>
-            div {
-                display: flex;
-                justify-content: center;
-                align-items: center;
+            .sunny {
+                background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQREayfm8mV1QonfqBx8pFVuZ5_DygVUi13JzxQFTzs-tIUwRDv);
+                background-size: cover;
+                width: 100px;
+                height: 100px;
             }
-            .black-coffee {
-                height: 70vh;
-                color: white;
-                background-color: black;
-
+            .cloudy {
+                background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr4luZFsUQeEZ5lIexKU8fINIQPP9LSymaWwGapeXvrrleqgJq);
+                background-size: cover;
+                width: 100px;
+                height: 100px;
             }
-            .latte {
-                height: 70vh;
-                color: white;
-                background-color: burlywood;
+            .rainy {
+                background-image: url(https://upload.wikimedia.org/wikipedia/hu/8/86/Es%C5%91_szimb%C3%B3lum.png);
+                background-size: cover;
+                width: 140px;
+                height: 120px;
             }
-            .tea {
-                height: 70vh;
-                color: white;
-                background-color: activecaption;
-
-            }
-            p{
-                font-size: 40px;
-
-            }
-
+            
         </style>
     </head>
     <body>
-        <h1>Ka gersiu?</h1>
-        <div class="<?php print $coffee; ?>"></div> 
-        <p>Gersiu <?php print $coffee; ?></p>
+        <h1>Koks šiandien oras?</h1>
+        <div class="<?php print $class; ?>"></div> 
+        <p><?php print $koks_siandien_oras; ?></p>
     </body>
 </html>
