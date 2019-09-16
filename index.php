efde03394cff8a16851ac8ef8d330a9de76505e7
raseliet <rasele27@gmail.com>
2019-09-16 12:41:55 +0300

    Wallet virsytas - v2.

--- index.php
+++ index.php
@@ -3,13 +3,18 @@ $months = 12;
 $wallet = 1000;
 $month_income = 700;
 
-
 for ($i = 1; $i <= 12; $i++) {
-    $month_expenses = rand(500, 900);
+    $month_expenses = rand(600, 1200);
     $wallet += $month_income - $month_expenses;
+
+    if ($wallet < 0) {
+        $h2_text = "Atsargiai, $months mėnesį gali baigtis pinigai: $wallet";
+        break;
+    }
+
+    $h2_text = "Po $months m., prognozuojamas likutis: $wallet";
 }
 
-$h2_text = "Po $months m., prognozuojamas likutis: $wallet";
 ?>
 <html>
     <head>
