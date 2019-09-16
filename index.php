8414b051b3704b13f8d1b03676a4ee5e5dbaaaef
raseliet <rasele27@gmail.com>
2019-09-16 11:05:11 +0300

    Katašunių išeiga - v1.

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$pavyko = rand(0, 1);
+$katasuniai = 0;
 
-$max = rand(0, 6);
-
-for ($x = 0; $x < $max; $x++) {
-    print "Tai yra $x-tasis ciklas";
+for ($i = 1; $i <= $kates; $i++) {
+    for ($j = 1; $j < $sunys; $j++) {
+        $pavyko = rand(0, 1);
+        if ($pavyko) {
+            $katasuniai++;
+            break;
+        }
+    }
 }
-
 ?>
+<html>
+    <head>
+        <title>Katašunių išeiga</title> 
+    </head>
+    <body>
+        <h1>Katašunių išeiga</h1>
+        <h2>Dalyvavo <?php print $kates . ' katės ir ' . $sunys . ' šunys'; ?></h2>
+        <h3>Katašunių išeiga: <?php print $katasuniai; ?></h3>
+    </body>
+</html>
+
+
+
+
+
