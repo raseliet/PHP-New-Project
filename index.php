50930edc3b51b80161cc724d5726b24920191993
raseliet <rasele27@gmail.com>
2019-09-17 11:20:30 +0300

    Kiek dienų galėsi valgyti grikius? - v1.

--- index.php
+++ index.php
@@ -1,29 +1,17 @@
 <?php
-$days = 365;
-$pack_price = 3.50;
-$count_ttl = 0;
+$days = 0;
+$grikiai = 5000; //g
+$grikiu_likutis = $grikiai;
 
-for ($i = 0; $i < $days; $i++) {
-    $savaites_diena = date('N', strtotime("+$i days"));
-    //print "$savaites_diena<br/>";
-    if ($savaites_diena <= 5) {
-        $cigs_mon_fri = rand(3, 4);
-        $count_ttl += $cigs_mon_fri;
-        //print "$savaites_diena-ieni surukiau $cigs_mon_fri<br/>";
-    } elseif ($savaites_diena == 6) {
-        $cigs_sat = rand(10, 20);
-        $count_ttl += $cigs_sat;
-        //print "$savaites_diena-ieni surukiau $cigs_mon_fri<br/>";
-    } else {
-        $cigs_sun = rand(1, 3);
-        $count_ttl += $cigs_sun;
-        //print "$savaites_diena-ieni surukiau $cigs_mon_fri<br/>";
-    }
-}
-//print "$count_ttl <br/>";
-$price_ttl = $pack_price / 20 * $count_ttl;
 
-$h2_text = "Per $days dienas surūkysiu $count_ttl cigaretes už $price_ttl eur.";
+for ($per_day = rand(200, 500); $grikiu_likutis >= $per_day; $per_day = rand(200, 500)) {
+    $grikiu_likutis -= $per_day;
+    $days++;
+};
+
+$data = date('Y-m-d', strtotime("+ $days day"));
+$h2_text = "Rasta grikių: $grikiai g.";
+$h3_text = "Išgyvensiu dar $days dienas, iki $data.";
 ?>
 
 <html>
@@ -31,7 +19,8 @@ $h2_text = "Per $days dienas surūkysiu $count_ttl cigaretes už $price_ttl eur.
         <meta charset="utf-8">
     </head>
     <body>
-        <h1>Mano dūmų skaičiuoklė</h1>
+        <h1>Kiek dienų galėsi valgyti grikius?</h1>
         <h2><?php print $h2_text; ?></h2>
+        <h2><?php print $h3_text; ?></h2>
     </body>
 </html>
