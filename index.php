481df25a32718eb97fcc85c435032776786aa46f
raseliet <rasele27@gmail.com>
2019-09-17 11:42:41 +0300

    Kiek dienų galėsi valgyti grikius? - Dainiaus versija.

--- index.php
+++ index.php
@@ -1,26 +1,21 @@
 <?php
+
+$grikiai = 5000;
 $days = 0;
-$grikiai = 5000; //g
-$grikiu_likutis = $grikiai;
 
+for($i = rand(200, 500); $i < $grikiai; $i += rand(200, 500), $days++) {
+    var_dump("Diena: $days, Grikių likutis: $grikiai, Suvalgyti grikiai: $i");
+}
 
-for ($per_day = rand(200, 500); $grikiu_likutis >= $per_day; $per_day = rand(200, 500)) {
-    $grikiu_likutis -= $per_day;
-    $days++;
-};
+$data = date('Y/m/d', strtotime("+$days days"));
+$text_h2 = "Rasta grikių: $grikiai g.";
+$text_h3 = "Išgyvensiu dar $days dienas, iki $data";
 
-$data = date('Y-m-d', strtotime("+ $days day"));
-$h2_text = "Rasta grikių: $grikiai g.";
-$h3_text = "Išgyvensiu dar $days dienas, iki $data.";
 ?>
-
 <html>
-    <head>
-        <meta charset="utf-8">
-    </head>
     <body>
         <h1>Kiek dienų galėsi valgyti grikius?</h1>
-        <h2><?php print $h2_text; ?></h2>
-        <h2><?php print $h3_text; ?></h2>
+        <h2><?php print $text_h2; ?></h2>
+        <h3><?php print $text_h3; ?></h3>
     </body>
 </html>
