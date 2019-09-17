c0f102927d8aeda42f633c6fdb3a4847a36143ba
raseliet <rasele27@gmail.com>
2019-09-17 13:31:17 +0300

    Kada galėsiu nupirkti mašina? - v.1

--- index.php
+++ index.php
@@ -1,25 +1,29 @@
 <?php
-$months = 24;
-$car_price_new = 30000; //eur
-$depreciation = 2; // %/men
-//(% nuo esamos vertės per mėnesį)
-$car_price_used = $car_price_new;
 
-for ($i = 0; $i < $months; $i++) {
+$months = 0;
+$car_price_new = 30000;
+$depreciation = 2;
+$santaupos = 15000;
+
+for ($car_price_used = $car_price_new, $months = 0; $santaupos < $car_price_used; $months++) {
     $car_price_used -= ($car_price_used / 100 * $depreciation);
 }
 
-$depr_perc = 100 - ($car_price_used / $car_price_new * 100);
+$depr_perc = round(100 - ($car_price_used / $car_price_new * 100));
 
-$text_h2 = "Naujos mašinos kaina: $car_price_new eur.";
-$text_h3 = "Po $months mėn., mašinos vertė bus: $car_price_used eur.";
-$text_h4 = "Mašina nuvertės $depr_perc proc.";
+$h2_text = "Naujos mašinos kaina: $car_price_new";
+$h3_text = "Mašiną galėsi nusipirkti po $months mėn., kai jos vertė bus: $car_price_used eur.";
+$h4_text = "Mašina nuvertės $depr_perc proc";
+
 ?>
 <html>
-    <body>
-        <h1>Kiek nuvertės mašina?</h1>
-        <h2><?php print $text_h2; ?></h2>
-        <h3><?php print $text_h3; ?></h3>
-        <h4><?php print $text_h4; ?></h4>
-    </body>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Kiek nuvertės mašina?</h1>
+    <h2><?php print $h2_text; ?></h2>
+    <h3><?php print $h3_text; ?></h3>
+    <h4><?php print $h4_text; ?></h4>
+</body>
 </html>
