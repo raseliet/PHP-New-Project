8e57078c130d0cd56bcf77b2119b8576b36b5260
raseliet <rasele27@gmail.com>
2019-09-18 16:44:25 +0300

    Array weekdays stringais- Aurimo - v1.

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
-for ($i = 0; $i < 7; $i++) {
-    $array[$i] = date('l', strtotime("+ $i days"));
+$array = [];
 
-    if ($array[$i] == 'Saturday' || $array[$i] == 'Sunday') {
-        $array[$i] = 'Weekend';
+for ($i = 0; $i < 7; $i++) {
+    $day = date('l', strtotime("+ $i days"));
+
+    if ($day == 'Saturday' || $day == 'Sunday') {
+        $array[$day] = 'Weekend';
+    } else {
+        $array[$day] = 'Workday';
     }
-};
+}
 
 var_dump($array);
