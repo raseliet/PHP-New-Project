bb7d11bea14b1744b3d2a24ceb4ff2188c612602
raseliet <rasele27@gmail.com>
2019-09-18 10:37:13 +0300

    Array pirmas - v1.

--- index.php
+++ index.php
@@ -1,29 +1,24 @@
 <?php
 
-$months = 0;
-$car_price_new = 30000;
-$depreciation = 2;
-$santaupos = 15000;
+$fruits_array = ['obuolys', 'apelsinas', 'cukinija'];
+var_dump($fruits_array);
 
-for ($car_price_used = $car_price_new, $months = 0; $santaupos < $car_price_used; $months++) {
-    $car_price_used -= ($car_price_used / 100 * $depreciation);
-}
+$maisto_prekes_array = [
+    [
+        'pavadinimas' => 'Kiaušinis',
+        'kiekis' => 2,
+        'dydis' => 'didelis',
+    ],
+    [
+        'pavadinimas' => 'Baklažanas',
+        'kiekis' => 1,
+        'dydis' => 'mažas',
+    ],
+    [
+        'pavadinimas' => 'Grietinė',
+        'kiekis' => 1,
+        'dydis' => 'didelis',
+    ],
+];
 
-$depr_perc = round(100 - ($car_price_used / $car_price_new * 100));
-
-$h2_text = "Naujos mašinos kaina: $car_price_new";
-$h3_text = "Mašiną galėsi nusipirkti po $months mėn., kai jos vertė bus: $car_price_used eur.";
-$h4_text = "Mašina nuvertės $depr_perc proc";
-
-?>
-<html>
-<head>
-    <meta charset="UTF-8">
-</head>
-<body>
-    <h1>Kiek nuvertės mašina?</h1>
-    <h2><?php print $h2_text; ?></h2>
-    <h3><?php print $h3_text; ?></h3>
-    <h4><?php print $h4_text; ?></h4>
-</body>
-</html>
+var_dump($maisto_prekes_array);
