0a5967a3102369d8a1ee4cdbdba87c83848c3689
raseliet <rasele27@gmail.com>
2019-09-18 12:59:37 +0300

    Array penktas - mano atmintis - flashback - v1.

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ $mano_atmintis = [
 ];
 
 $h2_text = "$vardas atmintis";
+$rand_flashback_idx = rand(0, count($mano_atmintis) - 1);
+
 ?>
 
 <html>
@@ -17,9 +19,10 @@ $h2_text = "$vardas atmintis";
         <h1>Kas buvo penktadienį?</h1>
         <h2><?php print $h2_text; ?></h2>
         <ul>
-            <?php foreach ($mano_atmintis as $prisiminimas): ?>
+<?php foreach ($mano_atmintis as $prisiminimas): ?>
                 <li><?php print $prisiminimas; ?></li>
-            <?php endforeach; ?>
+<?php endforeach; ?>
         </ul>
+        <h3><?php print $mano_atmintis[$rand_flashback_idx]; ?></h3>
     </body>
 </html>
