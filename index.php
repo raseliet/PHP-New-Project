a03a96cd5265460aa0bc56c303572e8808dc9575
raseliet <rasele27@gmail.com>
2019-09-19 15:21:22 +0300

    Array korekcijos per indeksus su foreach - v2.

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
- 
+
 $array = [
-'Monday' =>'Monday',
- 'Tuesday' => 'Tuesday',
- 'Wednesday' => 'Wednesday',
- 'Thursday' => 'Thursday',
- 'Friday' => 'Friday',
- 'Saturday' => 'Saturday',
- 'Sunday' => 'Sunday'
+    'Monday' => 'Monday',
+    'Tuesday' => 'Tuesday',
+    'Wednesday' => 'Wednesday',
+    'Thursday' => 'Thursday',
+    'Friday' => 'Friday',
+    'Saturday' => 'Saturday',
+    'Sunday' => 'Sunday'
 ];
 
-$array[0] = 'darbo diena';
-$array[1] = 'darbo diena';
-$array[2] = 'darbo diena';
-$array[3] = 'darbo diena';
-$array[4] = 'darbo diena';
-$array[5] = 'savaitgalis';
-$array[6] = 'savaitgalis';
+foreach ($array as $key => $day) {
+    if ($key == 'Friday') {
+        print "$key yra gera diena";
+    }
+}
 
 var_dump($array);
