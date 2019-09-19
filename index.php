b5e0453957a22bb9300d278a1570277e280d00e5
raseliet <rasele27@gmail.com>
2019-09-19 06:51:36 +0300

    Array Mano ir Draugo atmintis - v1.

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <?php
+$draugo_vardas = 'Draugo';
+$draugo_atmintis = [
+    'Penktadienis', 'Paskaita', 'Kinas',
+    'Vynas', 'Alus', 'Knygos', 'Paskaita'
+];
 
-$array = [];
+$mano_vardas = 'Rasos';
+$mano_atmintis = [
+    'Penktadienis', 'Paskaita', 'Baras',
+    'Viskis', 'Alus', 'Degtine', 'Paskaita'
+];
 
-for ($i = 0; $i < 7; $i++) {
-    $day = date('l', strtotime("+ $i days"));
 
-    if ($day == 'Saturday' || $day == 'Sunday') {
-        $array[$day] = 'Weekend';
-    } else {
-        $array[$day] = 'Workday';
-    }
-}
+$bendra_atmintis = [];
 
-var_dump($array);
+$h2_text = "$draugo_vardas atmintis: ";
+$h3_text = "$mano_vardas atmintis: ";
+$h4_text = "Sutapę prisiminimai";
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Kas buvo penktadienį?</h1>
+        <h2><?php print $h2_text; ?></h2>
+        <ul>
+            <?php foreach ($mano_atmintis as $mprisiminimas): ?>
+                <li><?php print $mprisiminimas; ?></li>
+
+            <?php endforeach; ?>
+        </ul>
+        <h2><?php print $h3_text; ?></h2>
+        <ul>
+            <?php foreach ($draugo_atmintis as $dprisiminimas): ?>
+                <li><?php print $dprisiminimas; ?></li>
+            <?php endforeach; ?>
+        </ul>
+
+
+    </body>
+</html>
