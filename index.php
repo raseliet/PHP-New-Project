b8e20f8b68d6afdddfb80ad3e70b3915ee9ef6c7
raseliet <rasele27@gmail.com>
2019-09-19 14:10:43 +0300

    Array Bank amount su HTML ir css - v1.

--- index.php
+++ index.php
@@ -1,33 +1,63 @@
 <?php
-
+​
 $bank_report = [
     [
-        'name' => 'IKI darbo užmokestis',
+        'operation_name' => 'IKI darbo užmokestis',
         'amount' => 600,
     ],
     [
-        'name' => 'Kalvarijų Načnykas',
+        'operation_name' => 'Kalvarijų načnykas',
         'amount' => -15,
     ],
     [
-        'name' => 'Pinigų išgryninimas Naugarduko g.',
-        'amount' => -10000,
+        'operation_name' => 'Viešbutis Panorama',
+        'amount' => -20,
     ],
     [
-        'name' => 'Pinigų išgryninimo mokestis',
-        'amount' => -100000000,
+        'operation_name' => 'UAB Misterijos fėjos',
+        'amount' => -55,
     ],
 ];
-
-foreach ($bank_report as $key => $value) {
-
-    if ($value['amount'] > 0) {
-        $bank_report[$key]['css_class'] = 'expense';
+​
+foreach ($bank_report as $operation_idx => $operation) {
+    if ($operation['amount'] > 0) {
+        $bank_report[$operation_idx]['css_class'] = 'income';
     } else {
-        $bank_report[$key]['css_class'] = 'income';
-      
+        $bank_report[$operation_idx]['css_class'] = 'expense';
     }
+    
+    $bank_report[$operation_idx]['text'] = "{$operation['operation_name']}: {$operation['amount']}";
 }
-var_dump($bank_report);
-print $bank_report[0]['amount'] < 0;
+​
+$text_h1 = 'Mano banko išklotinė';
+​
 ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP arrays examples</title>
+        <style>
+            .item {
+                width: 50%;
+            }
+            
+            .income {
+                background-color: green;
+            }
+​
+            .expense {
+                background-color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <h1><?php print $text_h1; ?></h1>
+        <ul>
+            <?php foreach ($bank_report as $operation): ?>
+                <li class="item <?php print $operation['css_class']; ?>">
+                    <?php print $operation['text']; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
+</html>
