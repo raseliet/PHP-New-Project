97ee23c89c80e0635e257ab0e3f0632f82436053
raseliet <rasele27@gmail.com>
2019-09-27 16:00:40 +0300

    Reference - sheep array - 4uzduotis v1.

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 
-$roll_joints = true;
+$sheep = ['blee'];
 
-$joint1 = &$roll_joints;
-$joint2 = &$joint1;
-$joint3 = &$joint2;
+for ($i = 0; $i < 5; $i++) {
+    $sheep[] = &$sheep[$i];
+}
 
-print $joint1; //1
-print $joint2; //1
-print $joint3; //1
+var_dump($sheep);
 
-
+//D:\Private\Doc\Desktop\www\index.php:12:
+//array (size=6)
+//  0 => string 'blee' (length=4)
+//  1 => string 'blee' (length=4)
+//  2 => string 'blee' (length=4)
+//  3 => string 'blee' (length=4)
+//  4 => string 'blee' (length=4)
+//  5 => string 'blee' (length=4)
