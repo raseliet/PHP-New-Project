36bce09d75de1031ed7909ae32a55961d4710070
raseliet <rasele27@gmail.com>
2019-09-30 14:26:03 +0300

    Formos - 3 uzduotis - buttono value keiciasi po kiekvieno submito ir prideda po paveiksliuka - su for ir divais - v1.

--- index.php
+++ index.php
@@ -13,16 +13,28 @@ if (isset($_POST['enter'])) {
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Formos</title>
+        <style>
+            .bananas {
+                display: inline-block;
+                width: 330px;
+                height: 330px;
+                background-image:url("https://images-na.ssl-images-amazon.com/images/I/51Ps3zmCS4L._SX322_BO1,204,203,200_.jpg");
+            }
+        </style>
         <meta charset="UTF-8">
 
     </head>
     <body>
         <form method="post">
-            <h1>Give it to me!</h1>
 
             <input type="submit" name="enter" value="<?php print $value; ?>">
 
-
         </form> 
+
+        <?php for ($i = 1; $i <= $value; $i++): ?>
+
+            <div class="bananas"></div>
+        <?php endfor; ?>
     </body>
 </html>
