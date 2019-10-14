801a008892213f11ccfcf476a4d820f2a8b6ddef
raseliet <rasele27@gmail.com>
2019-10-14 09:15:08 +0300

    Cleancode - Cookies client side ivedete per daug kartu ir button nebeaktyvus - v1

--- coockies.php
+++ coockies.php
@@ -1,19 +1,4 @@
 <?php
-////var_dump($_COOKIE);
-//
-//$string = 'verte';
-//setcookie('cookiename', $string, time() + 3600, '/');
-//
-//
-////setcookie('cookiename', 'myvalue', time() + 3600, '/');
-//
-////setcookie('mycookie', 'cookiedata', time(), '/');
-//////setcookie('mycookie', 'cookiedata', time() - 1, '/');
-//
-//var_dump($_COOKIE);
-
-
-​
 if (empty($_COOKIE)) {
     $id = rand(100000, 999999);
     $visits = 1;
@@ -21,15 +6,13 @@ if (empty($_COOKIE)) {
     $id = $_COOKIE['user_id'];
     $visits = ++$_COOKIE['visits'];
 }
-​
+
 setcookie('user_id', $id, strtotime('+30 days'));
 setcookie('visits', $visits, strtotime('+30 days'));
-​
+
 $h1_text = "User ID: $id";
 $h2_text = "Visits: $visits";
-​
 ?>
-
 <html>
     <head>
         <title>Cookie Visits Tracking</title>
@@ -39,7 +22,4 @@ $h2_text = "Visits: $visits";
         <h1><?php print $h2_text; ?></h1>            
     </body>
 </html>
-Collapse
-
-
 
