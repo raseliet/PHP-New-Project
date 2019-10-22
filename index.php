e808288af3e571079f5299bd2f198fd9faba9b62
raseliet <rasele27@gmail.com>
2019-10-22 12:02:37 +0300

    FileDB - v1

--- index.php
+++ index.php
@@ -1,59 +1,7 @@
 <?php
 
-session_start();
+require 'classes/FileDB.php';
 
+$FileDB = new FileDB('failas');
 
-require 'functions/file.php';
-
-if (!empty($_SESSION)) {
-    //prisijunges
-
-    $users = file_to_array('data/users.txt');
-    foreach ($users as $user) {
-        if ($user['email'] = $_SESSION['email']) {
-            $full_name = $user['full_name'];
-        }
-    }
-
-    $h1_text = "Sveikas atvykes, $full_name";
-} else {
-    $h1_text = 'Jus esate neprisijunges';
-}
-
-?>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>INDEX</title>
-
-    </head>
-
-    <style>
-        .container {
-            display: flex;
-            margin: 50px 0 0 0;
-            padding: 20px 0 0;
-            justify-content: left;
-        }
-        body {
-            background-image: url(https://media1.giphy.com/media/p1T5dtR0vhtXq/source.gif);
-            background-size: cover;
-        }
-        div {
-            display: inline-block;
-
-        }
-
-    </style>
-
-    <body>
-        <?php require 'navigation.php'; ?>
-        <div class="container">
-
-        </div>
-        <h1><?php print $h1_text; ?></h1>
-
-
-    </body>
-</html>
+var_dump($FileDB);
